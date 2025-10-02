71065392ad2200a7e987e7518badf5d503ef147b
Jeff Shumate <jeff@catapulsion.net>
2025-10-02 14:30:00 -0600

    add config loading and project settings

---
+++ pkg/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+// Configuration constants for checkpoint tool
+const (
+	ChangelogFileName = ".checkpoint-changelog.yaml"
+	InputFileName     = ".checkpoint-input"
+	DiffFileName      = ".checkpoint-diff"
+	StatusFileName    = ".checkpoint-status.yaml"
+	LockFileName      = ".checkpoint-lock"
+)
